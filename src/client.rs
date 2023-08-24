bcb005432a6911d3ae9d041ebb69c2a5b84d1429
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-08-23 22:42:37 -0400

    [internal] partial downloading on separate branch and code cleanup

--- src/client.rs
+++ src/client.rs
@@ -1,32 +1,34 @@
-use std::fs::File;
-use std::io::{Read, Write, BufReader, Cursor};
+use std::io::{Cursor, Read, Write};
 use std::net::TcpStream;
 
-use rodio::{OutputStream, Decoder, Sink, Source};
+use rodio::{Decoder, OutputStream, Sink};
 
 fn main() -> std::io::Result<()> {
     // Get a output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    let mut stream = TcpStream::connect("127.0.0.1:6969")?;
-
-    let mut buffer: Vec<u8> = Vec::new();
-    // 576 + 32 bits + 9, 17 or 32 bytes.
-    // 580 + 9, 17 or 32
-    // 589 or 597 or 612
-    
-    stream.read_to_end(&mut buffer)?;
-
-    println!("done");
-
-    let mut temp = File::create("temp.mp3")?;
-    temp.write(&mut buffer)?;
-
-    let source = Decoder::new_mp3(Cursor::new(buffer)).unwrap();
+    let song = buffer("output_no_meta.mp3")?;
 
     let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let source = Decoder::new_mp3(Cursor::new(song.clone())).unwrap();
+
     sink.append(source);
+
     sink.sleep_until_end();
 
     Ok(())
+}
+
+fn buffer(song: &str) -> std::io::Result<Vec<u8>> {
+    let mut stream = TcpStream::connect("127.0.0.1:6969")?;
+
+    stream.write(song.as_bytes())?;
+
+    let mut buffer: Vec<u8> = Vec::new();
+
+    stream.read_to_end(&mut buffer)?;
+
+    stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(buffer)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::fs::File;
-use std::io::BufReader;
-use std::thread;
-use std::net::{TcpStream, TcpListener, SocketAddrV4, Ipv4Addr};
 use std::io::prelude::*;
+use std::io::BufReader;
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
+use std::thread;
 
-use rodio::{OutputStream, Decoder, Sink};
+use rodio::{Decoder, OutputStream, Sink};
 
 const PORT: u16 = 6969;
 
@@ -16,65 +16,29 @@ fn main() -> std::io::Result<()> {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) =>{
-                thread::spawn(|| {
-                    handle_client(stream)
-                });
+            Ok(stream) => {
+                thread::spawn(|| handle_client(stream));
             }
-            Err(e) => {println!("{:?}", e);}
+            Err(e) => {
+                println!("{:?}", e);
+            }
         }
     }
 
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()>{
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut song_buffer = [0u8; 100];
+    let temp = stream.read(&mut song_buffer)?;
+    let song = String::from_utf8(song_buffer[0..temp].to_vec()).unwrap();
 
+    let mut file = BufReader::new(File::open(song)?);
 
-    
-    // Get a output stream handle to the default physical sound device
-    // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    // let file = File::open("output_no_meta.mp3").unwrap();
-    // Decode that sound file into a source
-    // let source = Decoder::new_mp3(file).unwrap();
-    
-    // let sink = Sink::try_new(&stream_handle).unwrap();
-    
-    // sink.append(source);
-    // sink.sleep_until_end();
-    let mut file = BufReader::new(File::open("output_no_meta.mp3").unwrap());
-    //
-    let meta = mp3_metadata::read_from_file("output_no_meta.mp3").unwrap();
-    
-    
-    let frame = &meta.frames[0];
-    let bitrate = frame.bitrate as u32 * 1000;
-    let samp_rate = frame.sampling_freq;
-    let samples = frame.size;
-    
-    let bps = samples as f64 / 8.0;
-    
-    let fsize = (bps * bitrate as f64 / samp_rate as f64 ) + if frame.padding {1.} else {0.};
-    println!("{}", fsize);
+    let mut buffer: Vec<u8> = Vec::new();
 
-    let mut buffer = [0u8; 288];
-    // let mut buffer: Vec<u8> = Vec::new();
-    let mut empty = false;
-    let mut temp = 0;
-    while !empty {
-        match file.read(&mut buffer)? {
-            n if n != 0 => {
-                stream.write(&buffer[0..n])?;
-                println!("sending {} bytes", n);
-                temp += 1;
-                if temp >= 400 {
-                    empty = true;
-                }
-            },
-            _ => empty = true,
-        };
-    }
+    file.read_to_end(&mut buffer)?;
+    stream.write(&buffer)?;
 
     println!("done");
 
