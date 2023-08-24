d3348b80ab7bebe6b79775ddaf2f1358cb2f11f6
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-08-24 19:57:33 -0400

    [client] super basic client, with searching, pausing and volume

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,9 @@
 use std::io::{Cursor, Read, Write};
 use std::net::TcpStream;
+use std::rc::Rc;
 
 use rodio::{Decoder, OutputStream, Sink};
-use rspotify::model::SearchResult;
+use rspotify::model::{SearchResult, FullTrack};
 use rspotify::prelude::BaseClient;
 use rspotify::{ClientCredsSpotify, Credentials};
 
@@ -10,41 +11,114 @@ fn main() -> std::io::Result<()> {
     // Get a output stream handle to the default physical sound device
     // Arc<Sink>
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let sink = Rc::new(sink);
 
     let stdin = std::io::stdin();
     let mut stdout = std::io::stdout();
 
     let mut input = String::new();
-    print!("search: ");
-    stdout.flush()?;
 
-    stdin.read_line(&mut input)?;
+    loop {
+        print!("cmd: ");
+        stdout.flush()?;
 
-    let result = search(input.as_str());
+        stdin.read_line(&mut input)?;
+        match input.trim() {
+            "search" => {
+                input.clear();
 
-    for (index, (name, _)) in result.iter().enumerate() {
-        println!("{index} {name}");
+                print!("search: ");
+                stdout.flush()?;
+
+                stdin.read_line(&mut input)?;
+                let result = search(input.trim());
+                input.clear();
+
+                // TODO improve
+                for (index, track) in result.iter().enumerate() {
+                    let name = &track.name;
+                    let artist = &track.artists[0].name;
+                    println!("{index} {name} by {artist} ");
+                }
+
+                print!("song_number: ");
+                stdout.flush()?;
+
+                stdin.read_line(&mut input)?;
+                print!("{}", input);
+                let song_number: usize = input.trim().parse::<usize>().unwrap();
+                input.clear();
+
+                // maybe serlize FullTrack and pased that throught instead of the id
+                let song_id = &result[song_number].id.to_owned().unwrap().to_string();
+                // if i use an rc I might be able to mutate the underlying data for partial loading
+                let song = buffer(&song_id)?;
+
+                let source = Decoder::new_mp3(Cursor::new(song.clone())).unwrap();
+
+                sink.play();
+                sink.append(source);
+                println!("{}", sink.len());
+            }
+            "skip" => {
+                input.clear();
+ 
+                println!("sorry broken");
+                // if sink.len() > 1 {
+                //     println!("{}", sink.len());
+                //     sink.skip_one();
+                //     println!("{}", sink.len());
+                //     sink.pause();
+                // } else {
+                //     //sink.clear();
+                // }
+            }
+            "temp" => {
+                input.clear();
+
+                println!("{}", sink.len());
+            }
+            "status" => {
+                input.clear();
+                println!("is paused {}", sink.is_paused());
+            }
+            "pause" => {
+                input.clear();
+
+                sink.pause();
+            }
+            "play" => {
+                input.clear();
+
+                sink.play();
+            }
+            "set volume" => {
+                input.clear();
+
+                print!("volume: ");
+                stdout.flush()?;
+
+                stdin.read_line(&mut input)?;
+
+                let volume = input.trim().parse::<f32>().unwrap();
+                input.clear();
+
+                sink.set_volume(volume);
+            }
+            "get volume" => {
+                input.clear();
+
+                println!("volume: {:.2}", sink.volume());
+            }
+            _ => {
+                input.clear();
+                println!("command not found");
+            }
+        }
     }
-
-    let mut input = String::new();
-    print!("Enter the number of song to play: ");
-    stdout.flush()?;
-
-    stdin.read_line(&mut input)?;
-
-    let song_number: usize = input.trim().parse::<usize>().unwrap();
-
-    let song = buffer(result[song_number].1.as_str())?;
-
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    let source = Decoder::new_mp3(Cursor::new(song.clone())).unwrap();
-
-    sink.append(source);
-
-    sink.sleep_until_end();
-
-    Ok(())
 }
 
 fn buffer(song: &str) -> std::io::Result<Vec<u8>> {
@@ -62,12 +136,12 @@ fn buffer(song: &str) -> std::io::Result<Vec<u8>> {
 }
 
 // fn search(input: &str) -> Vec<(String, Option<TrackId<'static>>)>{
-fn search(input: &str) -> Vec<(String, String)> {
+fn search(input: &str) -> Vec<FullTrack> {
     let creds = Credentials::from_env().unwrap();
     let spotify = ClientCredsSpotify::new(creds);
     spotify.request_token().unwrap();
 
-    let mut names: Vec<(String, String)> = Vec::new();
+    let mut names: Vec<FullTrack> = Vec::new();
     let search_resault = spotify
         .search(
             input,
@@ -80,10 +154,7 @@ fn search(input: &str) -> Vec<(String, String)> {
         .unwrap();
     match search_resault {
         SearchResult::Tracks(n) => {
-            names.reserve(n.items.len());
-            for i in n.items {
-                names.push((i.name, i.id.unwrap().into_static().to_string()));
-            }
+            names = n.items;
         }
         _ => {}
     }
